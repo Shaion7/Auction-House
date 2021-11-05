4b69dc5e1422574504eb472be2917084f5178730
Shaion7 <shaion5160@gmail.com>
2021-11-05 16:43:04 -0700

    Uploading AuctionHouse Spring Project

---
+++ AuctionHouse/src/main/java/com/auctionhouse/AuctionHouse/AuctionHouseApplication.java
@@ -0,0 +1,13 @@
+package com.auctionhouse.AuctionHouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionHouseApplication.class, args);
+	}
+
+}
---
+++ AuctionHouse/src/test/java/com/auctionhouse/AuctionHouse/AuctionHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auctionhouse.AuctionHouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
